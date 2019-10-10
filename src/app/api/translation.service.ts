d972de69320b4aec51fb6617207219d34d05adb8
student <student@example.com>
2019-10-10 13:34:15 +0200

    HttpClient used in translation service GET request implemented

--- src/app/api/translation.service.ts
+++ src/app/api/translation.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public getTranslation(userInput:string){
+    let url = 'https://api.mymemory.translated.net/get?q='
+    + userInput +
+    '&langpair=en|cs';
 
+    return this.http.get(url);  
   }
 }
