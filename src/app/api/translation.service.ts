9aa8e45a575c1ed770f6a4c27d9f8d3182f6a47a
student <student@example.com>
2019-10-10 13:24:06 +0200

    translation sevice generated

---
+++ src/app/api/translation.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslationService {
+
+  constructor() { }
+
+  public getTranslation(userInput:string){
+
+  }
+}
